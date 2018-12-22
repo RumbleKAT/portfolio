058ec4d679ec47738cc86057cbdd469c47a567d9
송명진 <songmyeongjin@songmyeongjin-ui-MacBook-Pro.local>
2018-12-22 14:37:42 +0900

    add callback in prev, next btn

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,6 @@ function Portfolio(state){
 Portfolio.prototype.btnEvtTrigger = function(){
     let btnPrev = document.getElementById('prev')
     let btnNext = document.getElementById('next')
-    
     btnPrev.addEventListener('click',()=>{
         console.log(this.state);
         if(this.state.pageIndex <= 0){
@@ -14,6 +13,7 @@ Portfolio.prototype.btnEvtTrigger = function(){
         }else{
             this.state.pageIndex -= 1;
         }
+        this.checkPage();
     });
 
     btnNext.addEventListener('click',()=>{
@@ -23,11 +23,13 @@ Portfolio.prototype.btnEvtTrigger = function(){
         } else {
             this.state.pageIndex += 1;
         }
+        this.checkPage();
     });
-
 }
 
-Portfolio.prototype.pushPage = function(){
+Portfolio.prototype.checkPage = function(){
+    console.log("checking page!!");
+    console.log(this.state.pageIndex);
 
 }
 
@@ -48,7 +50,7 @@ Portfolio.prototype.loadTemplate = function(){
         if (res.ok) {
             res.text().then(output => {
                 if(this.state.pageIndex === 0){
-                    output = output.replace('${name}', this.state.pageWriter)
+                    output = output.replace('${name}',this.state.pageArray[0].pageWriter)
                 }
                 main.root.innerHTML = output;
                 console.log("Rendering OK!")
@@ -64,8 +66,24 @@ Portfolio.prototype.loadTemplate = function(){
 
 var state = {
     pageIndex : 0,
-    pageWriter : "송명진",
-    pageArray : []
+    pageArray : [
+        {
+            type : -1,
+            pageWriter : "송명진"
+        },
+        {
+            type : 0,
+            titleIdx : '01',
+            title : '두더지를 잡아라',
+            date : '2017년 05월',
+            description : '두더지를 잡아라는 C++ 기반 오픈소스 라이브러리로 개발된 게임입니다. 시간에 따라 두더지와 토끼, 폭탄 두더지 등 다양한 게임요소들의 등장으로 단순함을 탈피하고 Microsoft Azure와의 연동으로 랭킹제를 제작했습니다.',
+            img : [
+                '0_01.png,','0_02.png','0_03.png'
+            ],
+            leftContent : 'https://github.com/RumbleKAT/Catch_Mouse',
+            rightContent : '역할 : 게임 클라이언트 개발자 \n 백엔드 개발자 \n 그래픽 디자이너'
+        }
+    ]
 };
 
 var main = new Portfolio(state);
